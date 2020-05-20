f072f35dc2b53643ee9c5b3f80456bf0fc9cdfe9
Nick van Eijndhoven <nickve.nl@gmail.com>
2020-05-20 23:40:51 +0200

    GetDxHistogram4 is basically complete now.

--- dt-test/code.cxx
+++ dt-test/code.cxx
@@ -412,3 +412,264 @@ TH1F GetDxHistogram3(TH1* hx,Int_t nc,Double_t dxbin,Double_t dxmin,Double_t dxm
  return hdx;
 }
 ///////////////////////////////////////////////////////////////////////////
+TH1F GetDxHistogram4(TH1* hx,Int_t nc,Double_t dxbin,Double_t dxmin,Double_t dxmax,Int_t mode,Double_t fact)
+{
+// Provide the interval size (dx) distribution of X-axis intervals between a certain
+// fixed amount of consecutive histogram entries of the specified input histogram.
+// This facility can for instance be used to investigate the distribution
+// of time intervals between observed events.
+//
+// Input arguments :
+// -----------------
+// hx    : The input histogram.
+// nc    : The step count to arrive at the required consecutive entry (see example below).
+// dxbin : The bin size of the X-axis for the dx distribution (see also the note below).
+//         dxbin=0  ==> Bin size taken the same as the input histogram "hx".
+//         dxbin=-1 ==> Bin size taken to be the minimal encountered dx interval,
+//                      or "fact" times the bin size of the input histogram "hx", if the latter
+//                      is the larger. This provides a protection against too small bin sizes,
+//                      and consequently a very large number of bins, in case an extremely small
+//                      dx interval value is encountered.
+//                      In case "fact<=0" and no dx>0 interval was encountered, the bin size of
+//                      the input histogram "hx" is taken. 
+//         dxbin=-2 ==> Bin size taken to be "nc" times the bin size of the input histogram "hx".
+// dxmin : The lower bound of the produced histogram.
+//         dxmin<0 ==> Lower bound taken to be the minimal encountered dx interval.
+// dxmax : The upper bound of the produced histogram.
+//         dxmax<0 ==> Upper bound taken to be the maximal encountered dx interval
+//                     increased with one additional bin size to contain the maximal
+//                     encountered dx interval value in the output histogram.
+// mode  : 0 ==> Bin contents of the input histogram "hx" are regarded as event counts
+//               (rounded to the nearest integer) with as x-value the center of the bin.
+//               Multiple entries in the same bin are treated as multiple events with exactly
+//               the same x-value, possibly resulting in interval values dx=0.
+//               See also note 1) below.
+//         1 ==> Same as "mode=0", but the x-value of each entry of the input histogram "hx" will be
+//               assigned to a random value within the corresponding bin. See note 1) below.  
+//         2 ==> Bin contents of the input histogram "hx" are treated as (weighted) values.
+//               Each filled bin is treated as a single event with as x-value the center of the bin.
+//         3 ==> Same as "mode=2", but the x-value of each entry of the input histogram "hx" will be
+//               assigned to a random value within the corresponding bin. See also note 1) below.
+// fact  : Multiplication factor applied to the bin size of the input histogram "hx" to provide
+//         a lower limit to the bin size of the produced Dx histogram when "dxbin=-1". See note 1) below.
+//
+// Returned object : The 1-D histogram (TH1F) containing the dx distribution.
+//
+// Default values : dxbin=-1, dxmin=-1, dxmax=-1, mode=1 and fact=1.
+//
+// Note : A too coarse bin size "dxbin" may lead to binning effects which may affect
+//        a statistical interpretation of the resulting histogram, for instance in the
+//        light of a comparison with known background distributions.
+//        In case the input histogram "hx" reflects an unbinned situation, i.e. not more
+//        than one single entry per bin, then "dxbin=0" assures that also the produced
+//        "dx histogram" will be free of binning effects.
+//        For sparsely populated histograms, the same may hold for "dxbin=-2", which has
+//        the advantage of having fewer bins which may reduce computing time when
+//        performing statistical analyses (e.g. randomised combinatorics) on it.
+//        It is advised to always first inspect the produced "dx histogram" before
+//        performing a statistics analysis on it.
+//
+// Example :
+// ---------
+// Histogram "hx" contains the counts of event times according to a Poisson distribution.
+// Specifying "nc=1" will provide the histogram of dt intervals between each consecutive event,
+// i.e. the distribution of time intervals between the events (1,2), (2,3), (3,4) etc.
+// Specifying "nc=2" will provide the histogram of dt intervals between every 2nd consecutive event,
+// i.e. the distribution of time intervals between the events (1,3), (2,4), (3,5) etc.
+// In this case of a Poissonian input histogram these produced dt distributions are known
+// as the Erlang distributions (see also class NcMath), for which each time interval contains
+// exactly "nc" events of which the last event occurs right at the end of the interval.
+//
+// Notes :
+// -------
+// 1) In case a certain bin of the input histogram "hx" contains several entries, all these
+//    entries are regarded as separate entities with exactly the same x-value.
+//    Consequently, this may result in (several) dx intervals of value 0, resulting in
+//    entries at 0 in the output histogram.
+//    In case one wants to avoid this effect, the binning of the input histogram "hx" could be
+//    chosen fine enough to reflect a basically unbinned situation.
+//    Another way to avoid this effect is to specify "mode=1", for which all entries in the
+//    input histogram "hx" will be assigned a random value within the corresponding bin, instead of
+//    the usual center of bin value. The "sensitivity" to this randomisation effect can be tuned
+//    by the input argument "fact" which controls the minimal bin size of the produced Dx histogram.
+//    Yet another way of avoiding this effect is to specify "mode=2" or "mode=3", which will treat
+//    every filled bin of the input histogram as a single event, irrespective of the bin content.
+//    These modes 2 or 3 allow the treatment of input histograms for which the bin contents do
+//    not represent event counts, e.g. histograms which have been rescaled or filled with weights.
+// 2) In case "dxmax<0" the actual bin size of the output histogram (slightly) differs from the
+//    provided one because of the addition of one or two bin size(s) to the auto-detected dxmax
+//    in order to prevent entries to fall outside the histogram range. 
+//    If one wants multiple output histograms resulting from multiple invokations of this function
+//    to have identical range and bin size, the user is advised to obtain the output histogram
+//    parameters from the first produced histogram and pass these to the subsequent invokations. 
+// 3) This member function is used recursively.
+
+ TH1F hdx;
+
+ if (mode<0 || mode>3) return hdx;
+
+ if (!hx) return hdx;
+
+ if (nc<1) return hdx;
+ 
+ Int_t nenhx=hx->GetEntries();
+ if (nenhx<=nc) return hdx;
+
+ Int_t idxbin=TMath::Nint(dxbin);
+ if (idxbin<-2) return hdx;
+
+ // Create the output histogram if all parameters have been specified or determined automatically.
+ // If not, this will be done at a recursive invokation (see below) once (some of) the
+ // parameters have been determined automatically from the input histogram.
+ if (dxmin>=0 && dxmax>=dxmin && dxbin>0)
+ {
+  Int_t nbins=1;
+  Double_t range=dxmax-dxmin;
+  if (range>dxbin) nbins=TMath::Nint(range/dxbin);
+  hdx.SetBins(nbins,dxmin,dxmax);
+
+  // Add histogram and axes titles
+  TString s;
+
+  s="Dx interval distribution between ";
+  s+=nc+1;
+  s+=" consecutive entries (nc=";
+  s+=nc;
+  s+=", mode=";
+  s+=mode;
+  s+=")";
+  hdx.SetNameTitle("DxHistogram",s.Data());
+
+  Double_t binwidth=hdx.GetXaxis()->GetBinWidth(1);
+  s.Form("Counts per bin of size %-10.3g",binwidth);
+  hdx.GetXaxis()->SetTitle("Dx interval");
+  hdx.GetYaxis()->SetTitle(s.Data());
+ }
+
+ NcRandom ran;
+
+ // Determine the minimum and maximum encountered dx or fill the output histogram
+ Double_t x1,x2,deltax;
+ Int_t nx1,nx2;
+ Double_t deltaxmin=0;
+ Double_t deltaxmax=0;
+ Bool_t found=kFALSE;
+ Int_t ndxcount=0;
+ Int_t jstart;
+
+ Int_t nbhx=hx->GetNbinsX();
+ Double_t value=0;
+ Double_t xlow=0;
+ Double_t xup=0;
+ Double_t bsize=0;
+ for (Int_t i=1; i<=nbhx; i++)
+ {
+  deltax=-1;
+  ndxcount=0;
+  xlow=hx->GetBinLowEdge(i);
+  bsize=hx->GetBinWidth(i);
+  xup=xlow+bsize;
+  x1=hx->GetBinCenter(i);
+  if (mode==1 || mode==3) x1=ran.Uniform(xlow,xup);
+  value=hx->GetBinContent(i);
+  nx1=0;
+  if (value) nx1=1;
+  if (mode<2) nx1=TMath::Nint(value);
+
+  while (nx1>0)
+  {
+   // Check for multiple counts (left) in this bin
+   jstart=i+1;
+   if (nx1>1) jstart=i;
+
+   for (Int_t j=jstart; j<=nbhx; j++)
+   {
+    xlow=hx->GetBinLowEdge(j);
+    bsize=hx->GetBinWidth(j);
+    xup=xlow+bsize;
+    x2=hx->GetBinCenter(j);
+    if (mode==1 || mode==3) x2=ran.Uniform(xlow,xup);
+    value=hx->GetBinContent(j);
+    nx2=0;
+    if (value) nx2=1;
+    if (mode<2) nx2=TMath::Nint(value);
+
+    if (j==i) nx2=nx1-1; // Counting within the same bin
+
+    // Empty bin
+    if (nx2<1) continue;
+
+    ndxcount+=nx2;
+
+    if (ndxcount>=nc)
+    {
+     deltax=fabs(x2-x1);
+     if (dxmin>=0 && dxmax>=dxmin && dxbin>0) // Output histogram has been initialised
+     {
+      hdx.Fill(deltax);
+     }
+     else // Auto-determination of the output histogram range
+     {
+       if (!found || deltax<deltaxmin) deltaxmin=deltax;
+       if (!found || deltax>deltaxmax) deltaxmax=deltax;
+     }
+     ndxcount=0;
+     found=kTRUE;
+     break;
+    }
+   }
+   nx1--;
+  }
+ }
+
+ // Check if a suitable configuration of entries was encountered
+ if (!found) return hdx;
+
+ // Check if a recursive call is needed to actually create and fill the output histogram
+ Int_t nen=hdx.GetEntries();
+ if (!nen)
+ {
+  // Set the bin size (if needed) for the output histogram
+  if (!idxbin) dxbin=hx->GetBinWidth(1);
+  if (idxbin==-1)
+  {
+   dxbin=(hx->GetBinWidth(1))*fact;
+   if (deltaxmin>0 && deltaxmin>dxbin) dxbin=deltaxmin;
+   if (dxbin<=0) dxbin=hx->GetBinWidth(1);
+  }
+  if (idxbin==-2)
+  {
+   dxbin=hx->GetBinWidth(1);
+   dxbin=dxbin*float(nc);
+  }
+
+  // Set the auto-determined range of the output histogram
+  if (dxmin<0)
+  {
+   dxmin=deltaxmin;
+   // Compensate for randomized x-values within the input histogram bins
+   if (mode==1 || mode==3)
+   {
+    bsize=hx->GetBinWidth(1);
+    dxmin=dxmin-2.*bsize;
+    if (dxmin<0) dxmin=0;
+   }
+  }
+  if (dxmax<0)
+  {
+   dxmax=deltaxmax+dxbin;
+   // Compensate for randomized x-values within the input histogram bins
+   if (mode==1 || mode==3)
+   {
+    bsize=hx->GetBinWidth(1);
+    dxmax=dxmax+2.*bsize;
+   }
+  }
+
+  // Invoke the recursive call to create and fill the output histogram
+  hdx=GetDxHistogram4(hx,nc,dxbin,dxmin,dxmax,mode,fact);
+ }
+
+ return hdx;
+}
+///////////////////////////////////////////////////////////////////////////
--- dt-test/test.cc
+++ dt-test/test.cc
@@ -20,34 +20,42 @@
  Float_t xmax=500;
  Int_t nbins=100000;
 
+ // Settings for the Dt histogram
+ Double_t deltatbin=-1;
+ Double_t deltatmin=-1;
+ Double_t deltatmax=-1;
+ Double_t fact=1;
+ Int_t nevtdt=2; // Number of events within a dt cell for which the inter-event dt statistics will be performed 
+
  // The arival time window histogram
  TH1F* ht=new TH1F("ht","Arrival times;Arrival time;Counts",nbins,xmin,xmax);
  Double_t tbin=ht.GetXaxis()->GetBinWidth(1);
  Double_t tmin=ht.GetXaxis()->GetXmin();
  Double_t tmax=ht.GetXaxis()->GetXmax();
 
+ // Background and signal rates
  Float_t brate=0.030; // The background event rate in Hz
- Float_t srate=0.000; // The signal event rate in Hz 
+ Float_t srate=0.005; // The signal event rate in Hz 
  Float_t tsig=0;      // The central signal arrival time
  Float_t dtsig=tbin*5.;  // Spread of the signal arrival time
- Int_t nevtdt=2;      // Number of events within a dt cell for which the inter-event dt statistics will be performed 
 
- Float_t rate=brate+srate;          // Total event rate
- Int_t nevt=int(brate*(xmax-xmin)); // The number background of events
- Int_t nsig=int(srate*(xmax-xmin)); // The number of signal events
+ Float_t rate=brate+srate;                  // Total event rate
+ Int_t nbkg=TMath::Nint(brate*(xmax-xmin)); // The number background of events
+ Int_t nsig=TMath::Nint(srate*(xmax-xmin)); // The number of signal events
 
- cout << " Event rates (Hz) Background:" << rate << " Signal:" << srate << " Total:" << rate << endl;
+ cout << " Event rates (Hz) Background:" << brate << " Signal:" << srate << " Total:" << rate << endl;
 
  Double_t nrandom=1e3; // (Maximum) number of randomised configurations for direct psi P-value determination (0 means n=1e19)
  Int_t ncut=10;        // Number of obtained psi>psi0 entries at which randomisations will be terminated (0= no early termination)
 
- // Fill the time window histogram with uniform event arrival times
+ // Fill the time window histogram with uniform background event arrival times
  // This will mimic a Poissonian background
  // Also a filling with identical time differences is possible
 
- Float_t step=(xmax-xmin)/float(nevt);
+ Float_t step=0;
+ if (nbkg) step=(xmax-xmin)/float(nbkg);
  Float_t x=xmin;
- for (Int_t i=0; i<nevt; i++)
+ for (Int_t i=0; i<nbkg; i++)
  {
   x=ran.Uniform(xmin,xmax); // Uniform filling
 ///  x=x+step; // Identical dt filling
@@ -65,35 +73,51 @@
  ht->Draw();
  
  // Create the delta t histogram
-
-/// TH1F hdt=lab.GetDxHistogram(ht,nevtdt,1,0,-1);
-/// TH1F hdt=lab.GetDxHistogram(ht,nevtdt,0,0,-1);
-///// TH1F hdt=lab.GetDxHistogram(ht,nevtdt,0,tbin,-1);
-/// TH1F hdt=GetDxHistogram2(ht,nevtdt,0,0,-1,0);
- TH1F hdt=GetDxHistogram3(ht,nevtdt,-1,0,-1,1);
+ TH1F hdt=lab.GetDxHistogram(ht,nevtdt,deltatbin,deltatmin,deltatmax);
+ TH1F hdt0=GetDxHistogram4(ht,nevtdt,deltatbin,deltatmin,deltatmax,0,fact);
+ TH1F hdt1=GetDxHistogram4(ht,nevtdt,deltatbin,deltatmin,deltatmax,1,fact);
+ TH1F hdt2=GetDxHistogram4(ht,nevtdt,deltatbin,deltatmin,deltatmax,2,fact);
+ TH1F hdt3=GetDxHistogram4(ht,nevtdt,deltatbin,deltatmin,deltatmax,3,fact);
  
- TCanvas* c2=new TCanvas("c2","c2");
+ TCanvas* c2=new TCanvas("c2","hdt");
  hdt.Draw();
+ 
+ TCanvas* c20=new TCanvas("c20","hdt0");
+ hdt0.Draw();
+ 
+ TCanvas* c21=new TCanvas("c21","hdt1");
+ hdt1.Draw();
+ 
+ TCanvas* c22=new TCanvas("c22","hdt2");
+ hdt2.Draw();
+ 
+ TCanvas* c23=new TCanvas("c23","hdt3");
+ hdt3.Draw();
 
- Int_t nbdt=hdt.GetNbinsX();
- Double_t deltatbin=hdt.GetXaxis()->GetBinWidth(1);
- Double_t deltatmin=hdt.GetXaxis()->GetXmin();
- Double_t deltatmax=hdt.GetXaxis()->GetXmax();
+ TH1F* hdtx=&hdt1;
+
+ Int_t nbdt=hdtx->GetNbinsX();
+ deltatbin=hdtx->GetXaxis()->GetBinWidth(1);
+ deltatmin=hdtx->GetXaxis()->GetXmin();
+ deltatmax=hdtx->GetXaxis()->GetXmax();
+
+ cout << " dtnbins:" << nbdt << " dtbsize:" << deltatbin << " dtmin:" << deltatmin << " dtmax:" << deltatmax << endl;
 
  // Creation of the Poisson based dt PDF for a background only hypothesis
  TF1 fdtbkg=math.PoissonDtDist(rate,nevtdt);
- fdtbkg.SetRange(0,deltatmax);
+ fdtbkg.SetRange(deltatmin,deltatmax);
 
  // Provide the dt PDF histogram with the same binning as the Delta t distribution
- fdtbkg.SetNpx(1000);
-//// fdtbkg.SetNpx(nbdt);
+ Int_t npx=1000;
+ npx=nbdt;
+ fdtbkg.SetNpx(npx);
  TH1* hpdfbkgdt=(TH1*)fdtbkg.GetHistogram()->Clone();
  hpdfbkgdt->SetName("hpdfbkgdt");
 
- Float_t hdtmedian=sample.GetMedian(&hdt);
+ Float_t hdtmedian=sample.GetMedian(hdtx);
  Float_t fdtmedian=sample.GetMedian(hpdfbkgdt);
 
- cout << " hdt median:" << hdtmedian << " pdfmedian:" << fdtmedian << endl;
+ cout << " hdtx median:" << hdtmedian << " pdfmedian:" << fdtmedian << endl;
 
  TCanvas* c3=new TCanvas("c3","c3");
  hpdfbkgdt->Draw();
@@ -102,29 +126,29 @@
  TString str="PIN";
  TH1F* hks=new TH1F("hks","KS pseudo experiment data",10,-1,1);
  cout << endl;
- lab.KolmogorovTest(str,&hdt,0,&fdtbkg,nrandom,hks,ncut);
+ lab.KolmogorovTest(str,hdtx,0,&fdtbkg,nrandom,hks,ncut);
  
  TCanvas* c4=new TCanvas("c4","c4");
  hks->Draw();
 
  // Bayesian psi value analysis of the Delta t distribution
  Double_t psidt=-1;
- psidt=math.PsiValue(&hdt,0,&fdtbkg);
+ psidt=math.PsiValue(hdtx,0,&fdtbkg);
 //@@@ psidt=math.PsiValue(&hdt,hpdfbkgdt);
-
+ 
  Float_t psimindt=-1;
  Float_t psimaxdt=-1;
- psimindt=math.PsiExtreme(&hdt,0,&fdtbkg,-2);
- psimaxdt=math.PsiExtreme(&hdt,0,&fdtbkg,-1);
-//@@@ psimindt=math.PsiExtreme(&hdt,hpdfbkgdt,0,-2);
-//@@@ psimaxdt=math.PsiExtreme(&hdt,hpdfbkgdt,0,-1);
+ psimindt=math.PsiExtreme(hdtx,0,&fdtbkg,-2);
+ psimaxdt=math.PsiExtreme(hdtx,0,&fdtbkg,-1);
+//@@@ psimindt=math.PsiExtreme(hdtx,hpdfbkgdt,0,-2);
+//@@@ psimaxdt=math.PsiExtreme(hdtx,hpdfbkgdt,0,-1);
 
  TH1F* hrpsidt=new TH1F("hrpsidt","Random #psi distr. for bkg hypothesis of event dt",1000,psimindt-1.,psimaxdt+1.);
 
  Double_t nrxdt=-1; // Returned number of actually performed randomizations
  Double_t pvaluedt=-1;
- pvaluedt=math.PsiPvalue(-1,nrandom,&hdt,0,&fdtbkg,0,0,hrpsidt,ncut,&nrxdt);
-//@@@ pvaluedtnu=math.PsiPvalue(-1,nrandom,&hdt,hpdfbkgdt,0,0,0,hrpsidtnu,ncut,&nrxdtnu);
+ pvaluedt=math.PsiPvalue(-1,nrandom,hdtx,0,&fdtbkg,0,0,hrpsidt,ncut,&nrxdt);
+//@@@ pvaluedtnu=math.PsiPvalue(-1,nrandom,hdtx,hpdfbkgdt,0,0,0,hrpsidtnu,ncut,&nrxdtnu);
  
  cout << endl;
  cout << " *** Psi analysis for the Poisson Dt distributions ***" << endl;
@@ -134,5 +158,4 @@
  
  TCanvas* c5=new TCanvas("c5","c5");
  hrpsidt->Draw();
-
 }
